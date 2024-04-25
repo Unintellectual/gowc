06e9abf1f261c1e804be507e36cf1e31e449565c
Noah Victoriano <noah.victoriano@cvsu.edu.ph>
2024-04-25 21:19:06 +0800

    made simlple wc

--- main.go
+++ main.go
@@ -1,6 +1,38 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
-	println("hello world")
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run wordcount.go <file>")
+		os.Exit(1)
+	}
+
+	filename := os.Args[1]
+
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("Error opening file: %s\n", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanWords)
+
+	wordCount := 0
+	for scanner.Scan() {
+		wordCount++
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Error reading file: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Word count: %d\n", wordCount)
 }
